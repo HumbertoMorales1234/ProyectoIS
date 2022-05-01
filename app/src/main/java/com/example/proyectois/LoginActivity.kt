d4572b2cfe1fa1b2fed8e83daa89f815d4401448
HUMBERTO MORALES <19121054@morelia.tecnm.mx>
2022-05-01 17:13:18 -0500

    Creación de la base del Login

---
+++ app/src/main/java/com/example/proyectois/LoginActivity.kt
@@ -0,0 +1,24 @@
+package com.example.proyectois
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.proyectois.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+}
+}
