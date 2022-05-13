282fed152662fcb4d65c46b9315fd457384822da
Sebastian Castro Villalobos <sebastiancastvill@gmail.com>
2022-05-13 11:08:43 -0500

    Creación de notificación básica y compatibilidad

--- app/src/main/java/com/example/proyectois/LoginActivity.kt
+++ app/src/main/java/com/example/proyectois/LoginActivity.kt
@@ -11,10 +11,16 @@ import kotlinx.android.synthetic.main.activity_login.*
 class LoginActivity : AppCompatActivity(){
 
    lateinit var btnSiguiente : Button;
+    lateinit var btnEntrar : Button;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login);
+
+        btnEntrar =findViewById(R.id.button3);
+        btnEntrar.setOnClickListener{
+            startActivity(Intent(this,Medicinas::class.java))
+        }
 
         btnSiguiente = findViewById(R.id.creaCuenta);
         btnSiguiente.setOnClickListener{
---
+++ app/src/main/java/com/example/proyectois/Medicinas.kt
@@ -0,0 +1,67 @@
+package com.example.proyectois
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+
+class Medicinas : AppCompatActivity() {
+    lateinit var botonNoti: Button;
+    //Creación del canal de manera global
+
+    val canalID = "chat"
+    val canalNombre = "chat"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_medicinas)
+
+        //Botón de prueba
+
+        botonNoti = findViewById(R.id.botonNoti);
+
+
+        //Verificar si la versión requiere canal, a partir de la versión "O (Oreo)" sí se requiere un canal
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val importancia = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(canalID,canalNombre,importancia)
+
+        //Se manda al manager de aplicaciones
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
+
+        //Configuración de la notificación
+        botonNoti.setOnClickListener{
+            //Configuración de la notificación
+            val notificacion = NotificationCompat.Builder(this, canalID).also { noti->
+            noti.setContentTitle("Titulo de prueba")
+            noti.setContentText("Texto de prueba")
+            noti.setSmallIcon(R.mipmap.logo_e_salud)
+
+            }.build()
+
+            val notificationManager = NotificationManagerCompat.from(applicationContext)
+            notificationManager.notify(1,notificacion)
+
+            }
+        }
+
+        }
+
+
+
+
+
+        //Construcción de la notificación
+
+
+
+
+    }
